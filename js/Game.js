fd29afc99afc1e7c1d8d332edfc782fb1531b32c
Mary Markeng <marymarkeng@gmail.com>
2020-10-10 13:13:37 +0100

    Adding methods active player and start game

--- js/Game.js
+++ js/Game.js
@@ -5,6 +5,10 @@ class Game {
         this.ready = false;
     }
     
+	get activePlayer() {
+        return this.players.find(player => player.active);
+	}
+
     createPlayers() {
         const players = [new Player('Player 1', 1, '#e15258', true),
                          new Player('Player 2', 2, '#e59a13')];
@@ -12,7 +16,9 @@ class Game {
         return players;
     }
 
-    startGame() {
-        
+    startGame(){
+        this.board.drawHTMLBoard();
+        this.activePlayer.activeToken.drawHTMLToken();
+        this.ready = true;
     }
 }
