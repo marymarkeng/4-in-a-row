ee7114afae6c0ddddaf4483a362affab8e676fd0
Mary Markeng <marymarkeng@gmail.com>
2020-10-10 11:30:07 +0100

    Adding constructors y generating objects

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,14 @@
 class Game {
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
     
+    createPlayers() {
+        const players = [new Player('Player 1', 1, '#e15258', true),
+                         new Player('Player 2', 2, '#e59a13')];
+                         
+        return players;
+    }
 }
