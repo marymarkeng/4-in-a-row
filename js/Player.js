0a79e969996ece7017fbfb5dfafd0a124a2ffff0
Mary Markeng <marymarkeng@gmail.com>
2020-10-10 12:50:45 +0100

    Getter methods

--- js/Player.js
+++ js/Player.js
@@ -17,4 +17,12 @@ class Player {
         
         return tokens;
     }
+
+    get unusedTokens(){
+        return this.tokens.filter(token => !token.dropped);
+    }
+
+	get activeToken() {
+        return this.unusedTokens[0];
+	}
 }
